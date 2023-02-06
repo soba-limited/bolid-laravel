43315d77f192187bfefbb6254ef1f271109bb72f
sobalimited <yamauchi.links@gmail.com>
2023-02-06 15:18:39 +0900

    order

--- app/Http/Controllers/LIndexController.php
+++ app/Http/Controllers/LIndexController.php
@@ -58,7 +58,7 @@ class LIndexController extends Controller
         ];
 
         $allarray = \Commons::LCommons($allarray);
-        return 'test';
+        return $this->jsonResponse($allarray);
     }
 
     public function sendMail(Request $request)
