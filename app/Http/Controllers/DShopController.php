33ac42d0a54a209f9e0559544155b601103a628b
sobalimited <yamauchi.links@gmail.com>
2023-03-16 17:45:51 +0900

    test7

--- app/Http/Controllers/DShopController.php
+++ app/Http/Controllers/DShopController.php
@@ -89,14 +89,6 @@ class DShopController extends Controller
     {
         $shop = new DShop;
 
-        if (!empty($request->s)) {
-            $shop = $shop->where('name', 'like', '%'.$request->s.'%')->orWhere('description', 'like', '%'.$request->s.'%');
-        } elseif (!empty($request->tag_id)) {
-            $shop = $shop::whereHas('DTags', function ($query) use ($request) {
-                $query->where('d_tag_id', $request->tag_id);
-            });
-        }
-
         if (!empty($sort)) {
             if ($sort == 'new') {
                 $shop = $shop->orderBy('id', 'desc');
@@ -115,6 +107,15 @@ class DShopController extends Controller
                 $shop = $shop->where('official_user_id', null);
             }
         }
+
+        if (!empty($request->s)) {
+            $shop = $shop->where('name', 'like', '%'.$request->s.'%')->orWhere('description', 'like', '%'.$request->s.'%');
+        } elseif (!empty($request->tag_id)) {
+            $shop = $shop::whereHas('DTags', function ($query) use ($request) {
+                $query->where('d_tag_id', $request->tag_id);
+            });
+        }
+
 
         $limit = 28;
         $skip = ($page - 1) * $limit;
