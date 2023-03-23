f7cbfcf83d606364057b7f4cdecf149a168650e7
sobalimited <yamauchi.links@gmail.com>
2023-03-23 13:21:41 +0900

    mail

--- resources/views/mail/bolides_japan_plan_add.blade.php
+++ resources/views/mail/bolides_japan_plan_add.blade.php
@@ -2,7 +2,7 @@
 
 <p>会社名：{{ $data['c_name'] }}</p>
 <p>役職名：{{ $data['position'] }}</p>
-<p>姓：{{ $data['name1'] }}　名：{{ $data['name2'] }}</p>
+<p>名前:{{ $data['name'] }}</p>
 <p>郵便番号：{{ $data['zipcode'] }}</p>
 <p>所在地：{{ $data['address'] }}</p>
 <p>電話番号：{{ $data['tel'] }}</p>
