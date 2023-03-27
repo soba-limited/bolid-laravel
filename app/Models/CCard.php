72bb15f9bb8316f9c08f07531830fca7a872b664
sobalimited <yamauchi.links@gmail.com>
2023-03-27 18:40:35 +0900

    iroiro

--- app/Models/CCard.php
+++ app/Models/CCard.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CCard extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $fillable = [
         'c_profile_id',
